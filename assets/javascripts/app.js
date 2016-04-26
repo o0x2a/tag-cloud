e5e83afce9cd477a87dbb01f2fb903c364ff74fd
Mehdi Tirgar <mehdi.tirgar@viaplay.com>
2016-04-26 22:45:56 +0200

    Fixed some lint issues and some imporoment for fronend script.

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,41 +1,46 @@
 'use strict';
+var $ = window.$ || window.jQuery;
 
-$(function() {
-  $('form').on('submit', function(e) {
+$(function () {
+  var submitBtn = $('.cloud-container button');
+  $('form').on('submit', function (e) {
+    var activePane = $('.tab-pane.active');
+    var type = activePane.attr('id');
+    var query = activePane.find('[type="text"]').val();
     e.preventDefault();
-    $('.cloud-container button').attr('disabled', true).text('Loading...');
-    var type = $('.tab-pane.active').attr('id');
-    var query = $('.tab-pane.active').find('[type="text"]').val();
+    if (!type || !query) { return; }
+    submitBtn.attr('disabled', true).text('Loading...');
     displayTags(type, query);
-  })
+  });
+
+  function displayTags(type, query) {
+    var tagToHtml = function (tag) {
+      return '<span class="word tag' + tag.size + '"><a href="#tag" title=' + tag.count + '"times">' + tag.word + '</a></span>';
+    };
+    var request = $.ajax({
+      url: '/api/' + type,
+      method: 'GET',
+      data: {q: query},
+      dataType: 'json'
+    });
+
+    request.done(function (tags) {
+      var tagsHtml = tags.map(tagToHtml);
+      $('#' + type + ' .tags').removeClass('hidden').html(tagsHtml);
+    });
+
+    request.fail(function (jqXHR, textStatus) {
+      var message = textStatus;
+      try {
+        message = JSON.parse(jqXHR.responseText).message;
+      } catch (e) {
+        console.error(jqXHR.responseText);
+      }
+      $('#error-alert').text('Request failed: ' + message).removeClass('hidden').show();
+    });
+
+    request.always(function () {
+      submitBtn.attr('disabled', false).text('Go!');
+    });
+  }
 });
-
-function displayTags(type, query) {
-  var tagToHtml = function(tag) {
-    return '<span class="word tag'+ tag.size +'"><a href="#tag" title='+ tag.count + '" times">' + tag.word + '</a></span>';
-  };
-  $('#error-alert').hide();
-  var request = $.ajax({
-    url: "/api/" + type,
-    method: "GET",
-    data: { q : query },
-    dataType: "json"
-  });
-
-  request.done(function( tags ) {
-    var tagsHtml = tags.map(tagToHtml);
-    $('#' + type + ' .tags').removeClass('hidden').html(tagsHtml);
-  });
-
-  request.fail(function( jqXHR, textStatus, data ) {
-    var message = textStatus;
-    try {
-      message = JSON.parse(jqXHR.responseText).message;
-    } catch(e) {}
-    $('#error-alert').text("Request failed: " +  message).removeClass('hidden').show();
-  });
-
-  request.always(function() {
-    $('.cloud-container button').attr('disabled', false).text('Go!')
-  });
-}
